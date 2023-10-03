3f48f6b6c526bfae5161574ca47a3514e4c15314
Yuna <yuna00708@gmail.com>
2023-10-03 12:25:45 +0900

    [feat] 채팅 html 코드 업데이트

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
 // import { Link, Routes, Route, useNavigate } from "react-router-dom";
 // import styled from "styled-components";
-// import axios from "axios";
-// import "./style.css";
+import axios from "axios";
+import "./style.css";
 
-function Chat() {}
+function Chat() {
+  return (
+    <ChatWrapper>
+      <ChatBox>
+        <div id="receiver">
+          <img src="images/coco.jpeg" alt="반려동물 프로필 사진" />
+          <div id="msg-box">
+            <p>안녕! 잘 지내? 누나 보고 싶어!</p>
+          </div>
+        </div>
+        <div id="sender"></div>
+      </ChatBox>
+      <SendBox>
+        <input type="text" id="sendMsg" />
+        <button id="sendBtn" type="submit">
+          Send
+        </button>
+      </SendBox>
+    </ChatWrapper>
+  );
+}
 
 export default Chat;
+
+const ChatWrapper = styled.div`
+  margin: auto;
+  width: 1185px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+const ChatBox = styled.div`
+  width: 900px;
+`;
+const SendBox = styled.div`
+  width: 900px;
+`;
