ea10057164b59db20558fa7d63fa9ce689ffc19e
Yuna <yuna00708@gmail.com>
2023-10-10 04:14:29 +0900

    [feat] 버튼 기능

--- src/pages/persona/PersonaSetting.jsx
+++ src/pages/persona/PersonaSetting.jsx
@@ -5,43 +5,36 @@ import axios from "axios";
 import "../style.css";
 
 function PersonaSetting() {
-  const [speciesName, setSpeciesName] = useState("");
   const navigate = useNavigate();
-  const [showTopButton, setShowTopButton] = useState(false);
-  const token = localStorage.getItem("token");
-
-  // 페이지 맨 위로 스크롤하는 함수
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
 
   // 이전 화면으로 이동
-  const goBack = () => {
+  const handleGoBack = () => {
     navigate(-1);
   };
 
   return (
     <SettingWrapper>
+      <GoBackBtn onClick={handleGoBack}>&lt;</GoBackBtn>
       <div id="personaStart">
         <h1>페르소나를 만들어 볼까요?</h1>
         <p>반려동물의 종을 선택해 주세요.</p>
       </div>
       <div id="cardContainer">
-        <img src="/images/card_dog.png" alt="반려동물은 강아지!" />
-        <ImageStyled src="/images/card_cat2.png" alt="반려동물은 고양이!" />
-        <img src="/images/card_bird.png" alt="반려동물은 새!" />
-        <img src="/images/card_ham.png" alt="반려동물은 햄스터!" />
-        <img src="/images/card_nothere.png" alt="여기 없어요. 직접 쓸래요" />
-      </div>
-      <div className="scroll-buttons">
-        {showTopButton && (
-          <button className="top-button" onClick={scrollToTop}>
-            Top
-          </button>
-        )}
+        <Link to="/pages/persona/PersonaDog">
+          <img src="/images/card_dog.png" alt="반려동물은 강아지!" />
+        </Link>
+        <Link to="/pages/persona/PersonaCat">
+          <ImageStyled src="/images/card_cat2.png" alt="반려동물은 고양이!" />
+        </Link>
+        <Link to="/pages/persona/PersonaBird">
+          <img src="/images/card_bird.png" alt="반려동물은 새!" />
+        </Link>
+        <Link to="/pages/persona/PersonaHam">
+          <img src="/images/card_ham.png" alt="반려동물은 햄스터!" />
+        </Link>
+        <Link to="/pages/persona/PersonaOthers">
+          <img src="/images/card_nothere.png" alt="여기 없어요. 직접 쓸래요" />
+        </Link>
       </div>
     </SettingWrapper>
   );
@@ -58,7 +51,21 @@ const SettingWrapper = styled.div`
   text-align: center;
 `;
 const ImageStyled = styled.img`
-  margin-top: 23px;
+  margin-top: 22px;
+`;
+const GoBackBtn = styled.button`
+  position: fixed;
+  z-index: 1000;
+  top: 150px;
+  left: 30px;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: #8bcef4;
+  color: white;
+  font-size: 25px;
+  border: none;
+  cursor: pointer;
 `;
 
 export default PersonaSetting;
