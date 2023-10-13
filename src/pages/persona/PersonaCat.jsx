0064ec9e1126064778489de9363e9fe1f2efbbe8
Yuna <yuna00708@gmail.com>
2023-10-13 16:02:37 +0900

    api update

--- src/pages/persona/PersonaCat.jsx
+++ src/pages/persona/PersonaCat.jsx
@@ -238,7 +238,37 @@ function PersonaCat() {
           <img src="/images/cat3.png" />
         </ImgContainer>
         <InputContainer>
-          <h1>고양이는 어떤 캐릭터인가요?</h1>
+          <h3>고양이는 어떤 캐릭터인가요?</h3>
+          <br />
+          <InputLabel id="secondChar">두 번째 특성</InputLabel>
+          <FormControl fullWidth variant="outlined" size="small">
+            <Select
+              variant="outlined"
+              value={charTwo}
+              onChange={(e) => setCharTwo(e.target.value)}
+              displayEmpty
+            >
+              <MenuItem value="" disabled>
+                <em>최대 두 개 선택</em>
+              </MenuItem>
+              <MenuItem value="애교가 많음">애교가 많은</MenuItem>
+              <MenuItem value="같이 있는 걸 좋아함">같이 있는 게 좋은</MenuItem>
+              <MenuItem value="다정함">다정한</MenuItem>
+              <MenuItem value="사교적임">사교적인</MenuItem>
+              <MenuItem value="예민함">예민한</MenuItem>
+              <MenuItem value="겁이 많음">겁이 많은</MenuItem>
+              <MenuItem value="호기심이 많음">호기심이 많은</MenuItem>
+              <MenuItem value="장난치는 게 좋음">장난치는 게 좋은</MenuItem>
+              <MenuItem value="외향적">외향적인</MenuItem>
+              <MenuItem value="활동적">활동적인</MenuItem>
+              <MenuItem value="독립적">독립적인</MenuItem>
+              <MenuItem value="바보 같음">바보 같은</MenuItem>
+              <MenuItem value="말이 많음">말이 많은</MenuItem>
+              <MenuItem value="호전적임">호전적인</MenuItem>
+              <MenuItem value="소심함">소심한</MenuItem>
+            </Select>
+          </FormControl>
+
           <Select
             label="좋아하는 놀이"
             variant="outlined"
